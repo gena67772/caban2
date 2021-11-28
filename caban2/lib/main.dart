0dfd906359d2294fa1a5f8981dc08abb17d74c1d
gena67772 <90128978+gena67772@users.noreply.github.com>
2021-11-28 11:23:22 +0300

    0.01

---
+++ caban2/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage(),);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+ const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column (children: [
+        Container(
+        width: 180,
+        height: 180,
+        color: Colors.green,
+        margin: const EdgeInsets.only( left: 150),
+        child: Row(children: [
+          Container (
+          width: 70,
+        height: 70,
+        color: Colors.blue,
+        margin: const EdgeInsets.only(right:5)),
+
+        Container(
+          width: 70,
+        height: 70,
+        color: Colors.blue,
+        margin: const EdgeInsets.only(left:5))],),),
+
+        Container(
+        width: 346,
+        height: 254,
+        color: Colors.green,
+        margin: const EdgeInsets.only(left: 120)),
+
+        Container(
+          width: 180,
+        height: 180,
+        color: Colors.green,
+        margin: const EdgeInsets.only( left: 150),
+          child: Row(children:[
+            Container(
+        width: 376,
+        height: 294,
+        color: Colors.green,
+        margin: const EdgeInsets.only(bottom: 22),)
+            ]
+          ,)
+        ),
+      ],)
+    );
+  }
+}
